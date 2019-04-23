0271d545afa55b8ba136ba91e906d071d47126ce
ellendhales <49896254+ellendhales@users.noreply.github.com>
2019-04-23 10:13:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -200,7 +200,7 @@
 			body : JSON.stringify(message)
 		};
 
-		let url = "http://hungry-students-csc337.herokuapp.com";
+		let url = "http://hungry-students-csc-337.herokuapp.com";
 
 		fetch(url, fetchOptions)
 			.then(checkStatus)
@@ -217,7 +217,7 @@
 	function showFavorites() {
 		console.log("entering show favorites");
 		clear();
-		let url = "http://hungry-students-csc337.herokuapp.com";
+		let url = "http://hungry-students-csc-337.herokuapp.com";
 
 		fetch(url, {method: "GET"})
 			.then(checkStatus)
